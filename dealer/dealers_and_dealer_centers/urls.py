09d70d45bd18a1de2de8d2c632b9d67a27f6eafb
Viktor Nikolaev <viktor.nikolaev@rubin.pro>
2021-12-07 16:13:10 +0300

    Добавление шаблонов

--- dealer/dealers_and_dealer_centers/urls.py
+++ dealer/dealers_and_dealer_centers/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import DealerList, VehicleList
+from .views import DealerList, DealerCenterList, VehicleNewList, VehicleWithMileageList
 
 urlpatterns = [
     path('', DealerList.as_view(), name='dealers'),
-    path('vehicles/', VehicleList.as_view(), name='vehicles')
+    path('dealer_and_dealer_centers/', DealerCenterList.as_view(), name='dealer_centers'),
+    path('new_vehicles/', VehicleNewList.as_view(), name='new_vehicles'),
+    path('vehicles_with_mileage/', VehicleWithMileageList.as_view(), name='vehicles_with_mileage'),
+
 ]
--- dealer/dealers_and_dealer_centers/views.py
+++ dealer/dealers_and_dealer_centers/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Dealer, DealerCenter, Vehicle
 
+
 class DealerList(ListView):
     model = Dealer
     template_name = 'dealers_and_dealer_centers/dealers_list.html'
@@ -15,16 +16,42 @@ class DealerList(ListView):
         context['title'] = 'Дилеры'
         return context
 
-class VehicleList(ListView):
-    model = Vehicle
-    template_name = 'dealers_and_dealer_centers/index.html'
-    context_object_name = 'vehicles'
+
+class DealerCenterList(DealerList):
+    model = DealerCenter
+    template_name = 'dealers_and_dealer_centers/dealer_centers_list.html'
+    context_object_name = 'dealer_centers'
 
     def get_queryset(self):
-        return Vehicle.objects.filter(archive=False)
+        return DealerCenter.objects.all()
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
+        context['title'] = 'Дилерские центры'
+        return context
+
+
+class VehicleNewList(ListView):
+    model = Vehicle
+    template_name = 'dealers_and_dealer_centers/vehicle_list.html'
+    context_object_name = 'vehicles'
+
+    def get_queryset(self):
+        return Vehicle.objects.filter(archive=False, vehicle_with_mileage=False)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Новые автомобили'
+        return context
+
+
+class VehicleWithMileageList(VehicleNewList):
+
+    def get_queryset(self):
+        return Vehicle.objects.filter(archive=False, vehicle_with_mileage=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Автомобили с пробегом'
         return context
 
