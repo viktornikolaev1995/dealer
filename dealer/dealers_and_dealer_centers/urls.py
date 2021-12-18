b80290236f94c4647df14507271aa282071a8ca8
7Viktor7 <viktornicolaev1995@gmail.com>
2021-12-18 17:29:55 +0300

    Внесение изменений в маршруты url

--- dealer/dealers_and_dealer_centers/urls.py
+++ dealer/dealers_and_dealer_centers/urls.py
@@ -20,12 +20,12 @@ urlpatterns = [
     re_path(r'^new_vehicles/$', VehicleNewList.as_view(), name='new_vehicles'),
     re_path(r'^new_vehicles_at_dealer_center/(?P<slug>[\D-]+)/$', VehicleNewAtDealerCenterList.as_view(),
             name='new_vehicles_at_dealer_center'),
-    re_path(r'^new_vehicle_at_dealer_center/(?P<slug>[\D-]+)/(?P<slug1>\D+-\w+-\D+-\d{4}(-\d*)?)/$',
+    re_path(r'^new_vehicle_at_dealer_center/(?P<slug>[\D-]+)/(?P<slug1>[\D-]+\d{4}(-\d*)?)/$',
             VehicleNewAtDealerCenterDetail.as_view(), name='new_vehicle_at_dealer_center'),
     re_path(r'^vehicles_with_mileage/$', VehicleWithMileageList.as_view(), name='vehicles_with_mileage'),
     re_path(r'^vehicles_with_mileage_at_dealer_center/(?P<slug>[\D-]+)/$',
             VehicleWithMileageAtDealerCenterList.as_view(), name='vehicles_with_mileage_at_dealer_center'),
-    re_path(r'^vehicles_with_mileage/(?P<slug>[\D-]+)/(?P<slug1>\D+-\w+-\D+-\d{4}-\d{6}(-\d*)?)/$',
+    re_path(r'^vehicles_with_mileage/(?P<slug>[\D-]+)/(?P<slug1>[\D-]+\d{4}-\d{3,6}(-\d*)?)/$',
             VehicleWithMileageAtDealerCenterDetail.as_view(), name='vehicle_with_mileage_at_dealer_center'),
     re_path(r'^create_review/(\d+)/$', AddDealerCenterReview.as_view(), name='create_review')
 
